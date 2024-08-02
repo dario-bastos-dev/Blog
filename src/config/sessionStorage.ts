521f4140781d535d08c8a579d599aaee1137ff08
Dario Bastos <dario.bastos.dev@gmail.com>
2024-08-02 09:25:53 -0300

    Sincronizando banco de dados

--- src/config/sessionStorage.ts
+++ src/config/sessionStorage.ts
@@ -7,6 +7,7 @@ const options = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
+	port: 3306
 };
 
 export const sessionStore = new MySQLStore(options);
